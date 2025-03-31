5903dee1ba34bb305d8c3f4a232c693f635752df
Jesper Eriksson Stenberg <jespermail@gmail.com>
2025-03-31 13:27:39 +0200

    about route

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require("express"); // Importera express för att skapa en server
+const cors = require("cors"); // Importera cors för att hantera CORS-problem
 const usersRouter = require("./routes/users"); // Importera användarrutten
+const aboutRouter = require("./routes/about"); // Importera om-rutten
 // const productsRouter = require("./routes/products"); // Importera produktrutten
 // const ordersRouter = require("./routes/orders"); // Importera orderrutten
 // const cartRouter = require("./routes/cart"); // Importera kundvagnsrutten
 
 const app = express(); // Skapa en express-app
 app.use(express.json()); // Middleware för att parsa JSON-data i inkommande förfrågningar
+app.use(cors()); // Använd cors för att tillåta alla domäner att göra förfrågningar till servern
 
-app.use("/users", usersRouter); // Använd användarrutten för alla förfrågningar som börjar med /users
-// app.use("/products", productsRouter); // Använd produktrouter för alla förfrågningar som börjar med /products
-// app.use("/orders", ordersRouter); // Använd orderrouter för alla förfrågningar som börjar med /orders
-// app.use("/cart", cartRouter); // Använd kundvagnrouter för alla förfrågningar som börjar med /cart
+app.use("/users", usersRouter); // Använd userRouter för alla förfrågningar som börjar med /users
+app.use("/about", aboutRouter); // Använd aboutRouter för alla förfrågningar som börjar med /about
+// app.use("/products", productsRouter); // Använd productsRouter för alla förfrågningar som börjar med /products
+// app.use("/orders", ordersRouter); // Använd ordersRouter för alla förfrågningar som börjar med /orders
+// app.use("/cart", cartRouter); // Använd cartRouter för alla förfrågningar som börjar med /cart
 
 const PORT = 5000; // Definiera porten för servern
 app.listen(PORT, () => { // Starta servern och lyssna på angiven port
