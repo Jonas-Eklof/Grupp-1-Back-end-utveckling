8e52e8755e2351816858e3f225ddead6f26f4b81
Bobbyloo89 <Mikael.lindstedt@live.se>
2025-04-01 10:24:14 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require("express"); // Importera express för att skapa en serve
 const cors = require("cors"); // Importera cors för att hantera CORS-problem
 const usersRouter = require("./routes/users"); // Importera användarrutten
 const aboutRouter = require("./routes/about"); // Importera om-rutten
-// const productsRouter = require("./routes/products"); // Importera produktrutten
-// const ordersRouter = require("./routes/orders"); // Importera orderrutten
-// const cartRouter = require("./routes/cart"); // Importera kundvagnsrutten
+const productsRouter = require("./routes/products"); // Importera produktrutten
+const ordersRouter = require("./routes/orders"); // Importera orderrutten
+const cartsRouter = require("./routes/carts"); // Importera kundvagnsrutten
+const authRouter = require("./routes/authRoutes");
 
 const app = express(); // Skapa en express-app
 app.use(express.json()); // Middleware för att parsa JSON-data i inkommande förfrågningar
@@ -12,9 +13,10 @@ app.use(cors()); // Använd cors för att tillåta alla domäner att göra förf
 
 app.use("/users", usersRouter); // Använd userRouter för alla förfrågningar som börjar med /users
 app.use("/about", aboutRouter); // Använd aboutRouter för alla förfrågningar som börjar med /about
-// app.use("/products", productsRouter); // Använd productsRouter för alla förfrågningar som börjar med /products
-// app.use("/orders", ordersRouter); // Använd ordersRouter för alla förfrågningar som börjar med /orders
-// app.use("/cart", cartRouter); // Använd cartRouter för alla förfrågningar som börjar med /cart
+app.use("/products", productsRouter); // Använd productsRouter för alla förfrågningar som börjar med /products
+app.use("/orders", ordersRouter); // Använd ordersRouter för alla förfrågningar som börjar med /orders
+app.use("/carts", cartsRouter); // Använd cartRouter för alla förfrågningar som börjar med /cart
+app.use(authRouter);
 
 const PORT = 5000; // Definiera porten för servern
 app.listen(PORT, () => { // Starta servern och lyssna på angiven port
